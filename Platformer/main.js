e3331c6ad83e86a02e09f544c9f77e5ba2b1ea76
Zach <kf.manik@gmail.com>
2016-04-06 19:33:21 +1000

    Added level art
    Added level file, level art tileset, and updated main to include draw
    code

--- Platformer/main.js
+++ Platformer/main.js
@@ -3,15 +3,89 @@
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
 
+//load tileset image
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+//Constants
+var LAYER_COUNT = 3; 
+var MAP = {tw:60, th:15};
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+
+var player = new Player();
+var keyboard = new Keyboard();
+
+function Run(params) 
+{
+    var deltaTime = getDeltaTime();
+    
+    
+    context.fillStyle = "#ccc";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    player.update(deltaTime);
+    player.draw();
+    
+    drawMap();
+    
+    fpsTime += deltaTime;
+    fpsCount++;
+    if(fpsTime >= 1)
+    {
+        fpsTime -= 1;
+        fps = fpsCount;
+        fpsCount = 0;        
+    }
+    context.fillStyle = "#f00";
+    context.font = "14px Arial";
+    context.fillText("FPS: " + fps,5,20,100);
+    
+}
+
+//DANGER ZOOOOONE
+function drawMap()
+{
+    for (var layerldx = 0; layerldx < LAYER_COUNT; layerldx++)
+    {
+        var idx = 0;
+        for (var y = 0; y < level1.layers[layerldx].height; y++)
+        {
+            for (var x = 0; x < level1.layers[layerldx].width; x++)
+            {
+                //do check
+                if(level1.layers[layerldx].data[idx] !=0 )
+                {
+                    var tileIndex = level1.layers[layerldx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+                    //                //image | sprite chunk to use          |       game space to draw
+                    context.drawImage(tileset,sx,sy,TILESET_TILE,TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+                    //console.log("Data was not empty at position:" + idx.toString());
+                }
+                //increment check index 
+                idx++;
+            }
+        }
+    }    
+}
+
+// This function will return the time in seconds since the function 
+// was last called
+// You should only call this function once per frame
+
 //DeltaTime vars and function 
 var startFrameMillis = Date.now();
 var endFrameMillis = Date.now();
 var fpsTime = 0;
 var fpsCount = 0;
 var fps = 0;
-// This function will return the time in seconds since the function 
-// was last called
-// You should only call this function once per frame
+
 function getDeltaTime()
 {
 	endFrameMillis = startFrameMillis;
@@ -31,40 +105,6 @@ function getDeltaTime()
 		
 	return deltaTime;
 }
-
-
-var player = new Player();
-var keyboard = new Keyboard();
-
-function Run(params) 
-{
-    var deltaTime = getDeltaTime();
-    
-    
-    context.fillStyle = "#ccc";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    
-    player.update(deltaTime);
-    player.draw();
-    
-    
-    fpsTime += deltaTime;
-    fpsCount++;
-    if(fpsTime >= 1)
-    {
-        fpsTime -= 1;
-        fps = fpsCount;
-        fpsCount = 0;        
-    }
-    context.fillStyle = "#f00";
-    context.font = "14px Arial";
-    context.fillText("FPS: " + fps,5,20,100);
-    
-}
-
-
-
-
 
 (function () {
     var onEachFrame;
